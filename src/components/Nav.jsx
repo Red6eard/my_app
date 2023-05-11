57fa97eac19cb46b8f167133768011bc73feb1e1
Red6eard <vadim.71rus@gmail.com>
2023-05-11 11:59:14 +0300

    доб.двух классов Элементу Пример В Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import style from "./Nav.module.css";
 const Nav = () => {
   return (
     <nav className={style.nav}>
-      <div className={style.item}>
+      <div className={`${style.item} ${style.center}`}>
         <a href="#">Profile</a>
       </div>
       <div className={style.item}>
