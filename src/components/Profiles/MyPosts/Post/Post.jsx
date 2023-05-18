5ca42ee41695aadc7391ff02adad117481b886d2
Red6eard <vadim.71rus@gmail.com>
2023-05-18 10:49:01 +0300

    добавлен аватар к постам

--- src/components/Profiles/MyPosts/Post/Post.jsx
+++ src/components/Profiles/MyPosts/Post/Post.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import style from "./Post.module.css";
 
 const Post = () => {
-  return <div className={style.post}>post 1</div>;
+  return (
+    <div className={style.post}>
+      <img src="https://www.w3schools.com/howto/img_avatar.png" alt="ava" />
+      post 1
+      <div>
+        <span>Like</span>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
