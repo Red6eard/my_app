dbacfcce4336d470fe38432e6970c0a15a058aba
Red6eard <vadim.71rus@gmail.com>
2023-05-22 11:01:43 +0300

    незначительные корректировки

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profiles/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+
+
 
 const App = () => {
   return (
