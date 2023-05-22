e6d3efd3848881ff301bcb1f1396fdf1a1d40462
Red6eard <vadim.71rus@gmail.com>
2023-05-22 11:28:10 +0300

    ссылки в Nav заменены на компонент  NavLink

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import style from "./Nav.module.css";
+import { NavLink } from "react-router-dom";
+
+
 
 const Nav = () => {
   return (
     <nav className={style.nav}>
       <div className={`${style.item} ${style.center}`}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile">Profile</NavLink>
       </div>
       <div className={`${style.item} ${style.center}`}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs">Messages</NavLink>
       </div>
       <div className={`${style.item} ${style.center}`}>
-        <a href="/news">News</a>
+        <NavLink to="/news">News</NavLink>
       </div>
       <div className={`${style.item} ${style.center}`}>
-        <a href="/music">Music</a>
+        <NavLink to="/music">Music</NavLink>
       </div>
       <div className={`${style.item} ${style.center}`}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/settings">Settings</NavLink>
       </div>
     </nav>
   );
