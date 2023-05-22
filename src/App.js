571c3d1e56114707bdd7ac92850a7d0a1d356138
Red6eard <vadim.71rus@gmail.com>
2023-05-22 10:57:59 +0300

    настроен роутинг для всех элементов навигации

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profiles/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
-    <div className="app-wrap">
-      <Header />
-      <Nav />
-      <div className="app-wrap-content">
-        <Dialogs />
-        {/* <Profile /> */}
+    <BrowserRouter>
+      <div className="app-wrap">
+        <Header />
+        <Nav />
+        <div className="app-wrap-content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
