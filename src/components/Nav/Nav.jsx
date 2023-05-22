fe64bb37a1a09fc12f8b63e1a0bb5a6ef7e10980
Red6eard <vadim.71rus@gmail.com>
2023-05-22 12:03:58 +0300

    добавлены стили для активного компонента nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,20 +7,20 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className={style.nav}>
-      <div className={`${style.item} ${style.center}`}>
-        <NavLink to="/profile">Profile</NavLink>
+      <div className={`${style.center}`}>
+        <NavLink to="/profile"className={navData => navData.isActive ? style.active : style.item} >Profile</NavLink>
       </div>
-      <div className={`${style.item} ${style.center}`}>
-        <NavLink to="/dialogs">Messages</NavLink>
+      <div className={` ${style.center}`}>
+        <NavLink to="/dialogs" className={navData => navData.isActive ? style.active : style.item}>Messages</NavLink>
       </div>
-      <div className={`${style.item} ${style.center}`}>
-        <NavLink to="/news">News</NavLink>
+      <div className={`${style.center}`}>
+        <NavLink to="/news" className={navData => navData.isActive ? style.active : style.item}>News</NavLink>
       </div>
-      <div className={`${style.item} ${style.center}`}>
-        <NavLink to="/music">Music</NavLink>
+      <div className={`${style.center}`}>
+        <NavLink to="/music" className={navData => navData.isActive ? style.active : style.item}>Music</NavLink>
       </div>
-      <div className={`${style.item} ${style.center}`}>
-        <NavLink to="/settings">Settings</NavLink>
+      <div className={`${style.center}`}>
+        <NavLink to="/settings" className={navData => navData.isActive ? style.active : style.item}>Settings</NavLink>
       </div>
     </nav>
   );
