5569c8573d5b3112036ffa92cc4c670714b781e1
Red6eard <vadim.71rus@gmail.com>
2023-06-07 16:00:00 +0300

    добавлены линки в диалогах

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import style from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogs_items}>
-        <div className={style.items}>Name 1</div>
-        <div className={style.items}>Name 2</div>
-        <div className={style.items}>Name 3</div>
-        <div className={style.items}>Name 4</div>
+        <div className={style.items}>
+          <NavLink to="/dialogs/1">Name 1</NavLink>
+        </div>
+        <div className={style.items}>
+          <NavLink to="/dialogs/2">Name 2</NavLink>
+        </div>
+        <div className={style.items}>
+          <NavLink to="/dialogs/3">Name 3</NavLink>
+        </div>
+        <div className={style.items}>
+          <NavLink to="/dialogs/4">Name 4</NavLink>
+        </div>
       </div>
       <div className={style.messages}>
         <div className={style.message}>Hi!</div>
