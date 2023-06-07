a41e3acf5a795f5015c9a45cf45c429a0ad35e0b
Red6eard <vadim.71rus@gmail.com>
2023-06-07 15:27:21 +0300

    правки для dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import style from "./Dialogs.module.css"
-
-
+import style from "./Dialogs.module.css";
 
 const Dialogs = () => {
-    return (
-        <div>
-            Dialogs
-        </div>
-    )
+  return (
+    <div className={style.dialogs}>
+      <div className={style.dialogs_items}>
+        <div className={style.items}>Name 1</div>
+        <div className={style.items}>Name 2</div>
+        <div className={style.items}>Name 3</div>
+        <div className={style.items}>Name 4</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>Hi!</div>
+        <div className={style.message}>How are you?</div>
+        <div className={style.message}>I'm OK!</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
