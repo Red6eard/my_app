513b5f7c98d6153264f11f4c676f7c4f6127f852
Red6eard <vadim.71rus@gmail.com>
2023-06-16 14:52:19 +0300

    настройка импортов для диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import style from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  return (
-    <div className={style.items}>
-      <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return <div className={style.message}>{props.message}</div>;
-};
+import DialogItem from "./DialogItem/DialogsItem";
+import Message from "./Message/Message";
 
 let dialogs = [
   {id: 1, name: "Name 1"},
