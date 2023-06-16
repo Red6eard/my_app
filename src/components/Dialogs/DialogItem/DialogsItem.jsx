e78fef147d0ca129a37a6482f3b0ba933b1f6d87
Red6eard <vadim.71rus@gmail.com>
2023-06-16 14:24:08 +0300

    диалоги разбиты на компоненты

---
+++ src/components/Dialogs/DialogItem/DialogsItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./../Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+  return (
+    <div className={style.items}>
+      <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+
+
+export default DialogItem;
